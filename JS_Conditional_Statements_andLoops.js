9bbc4f66fe4a3e0ea6b147718956acaba250df65
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-02-18 23:29:29 -0600

    Problem 8 complete but I do not understand this one at all

--- JS_Conditional_Statements_andLoops.js
+++ JS_Conditional_Statements_andLoops.js
@@ -86,5 +86,38 @@ else
         console.log(i);
 
     }
+} // Only question I have for this one is why did it they put it in the order of 3&5, then each individually?
+
+
+/* Question 8: According to Wikipedia a happy number is defined by the following process:
+    "Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers, while those that do not end in 1 are unhappy numbers (or sad numbers)".
+
+ Write a JavaScript program to find and print the first 5 happy numbers. */
+
+function happy_number(num) {
+    var m, n;
+    var c = [];
+
+    while (num != 1 && c[num] !== true) { // I am not sure what it means to have a variable next to an array with a value inside of it, and everything in this while loop is confusing
+        c[num] = true;
+        m = 0;
+        while (num > 0) {
+            n = num % 10;
+            m += n * n;
+            num = (num - n) / 10;
+        }
+        num = m;
+    }
+    return (num == 1);
 }
-// Only question I have for this one is why did it they put it in the order of 3&5, then each individually?
+
+var cnt = 5; // I don't understand this problem at all :/
+var num = 1;
+var f5 = '';
+while (cnt-- > 0) {
+    while (!happy_number(num))
+        num++;
+    f5 = f5 + (num + ',');
+    num++;
+}
+console.log('first 5 happy numbers are: ' + f5);

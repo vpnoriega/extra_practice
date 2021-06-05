a86210172efa3fd7ee2c179d9ec7a654628ece9c
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-06-05 17:07:46 -0500

    Fizz buzz with switch statement

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,27 +1,54 @@
-for (var i=1; i < 101; i++){
-    if (i % 15 === 0) console.log("FizzBuzz");
-    else if (i % 3 === 0) console.log("Fizz");
-    else if (i % 5 === 0) console.log("Buzz");
-    else console.log(i);
-}
+// Print integers 1 to N, but print “Fizz” if an integer is divisible by 3, “Buzz” if an integer is divisible by 5, and “FizzBuzz” if an integer is divisible by both 3 and 5.
 
-for (var i = 1; i<=100; i++){
-    if (i % 15 === 0) console.log("Fizzbuzz");
-    else if(i%3 === 0) console.log("fizz");
-    else if(i%5 === 0) console.log("buzz");
-    else console.log(i);
-}
-
-// this part doesn't work, only fizz buzz in each line:
-for(var i =1; i<=100; i++){
-    if(i%3 ===0){
-        console.log("buzz")
-    } else if (i%5 ===0) {
-        console.log("fizz")
-    } else if(i%3 === 0 && i % 5 ===0){
-        console.log("fizzbuzz")
-
+for(let n=1; n<50; n++){
+    if(n%15 === 0){
+        console.log(n + "Fizzbuzz")
+    } else if(n%5 === 0){
+        console.log(n +"buzz")
+    } else if(n%3 === 0){
+        console.log(n +"fizz")
     } else{
-        console.log(i);
+        console.log(n)
     }
 }
+
+for(let n=1; n<50; n++){
+    switch(true){
+        case n%5 === 0 && n%3 ===0:
+            console.log(n +"fizzbuzz");
+            break;
+        case n%5 === 0:
+            console.log(n +"buzz");
+            break;
+        case n%3 === 0:
+            console.log(n +"fizz");
+            break;
+        default:
+            console.log(n)
+    }
+}
+
+/** if (condition){
+    // executed only if "condition" is true
+} else if (other condition){
+    // executed only if "condition" was false and "other condition" is true
+} else {
+    // executed only if both "condition" and "other condition" were false
+}
+ */
+
+/**
+switch (true){ //Switch statement if the condition is True (similar to if statement)
+    case x: //(else if)
+        code block;
+        break;
+    case y: //(else if)
+        code block;
+        break;
+    case z: //(else if)
+        code block;
+        break;
+    default: //(else)
+        code block;
+}
+ */

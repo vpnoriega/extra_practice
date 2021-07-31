4c7ab9b837634dc4d665d2969f7e794e0a6a6d7f
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-07-30 23:47:37 -0500

    added refresher for logical values

--- js/jsPractice.js
+++ js/jsPractice.js
@@ -33,3 +33,16 @@ function trueOrFalse(wasThatTrue) {
 
 console.log(trueOrFalse(true));
 console.log(trueOrFalse(false));
+
+/** ***** **/
+//TODO: Replace the two if statements with one statement, using the && operator, which will return the string Yes if val is less than or equal to 50 and greater than or equal to 25. Otherwise, will return the string No.
+
+function testLogicalAnd(val) {
+    if (val <= 50 && val >=25) {
+        return "Yes";
+    }
+    return "No";
+}
+
+console.log(testLogicalAnd(30));
+console.log(testLogicalAnd(10));

f3cee289664ce21cc87eaa588055ed0d36bcf399
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-08-02 00:04:20 -0500

    Left off at 88 out of 111

--- js/jsPractice.js
+++ js/jsPractice.js
@@ -206,3 +206,6 @@ console.log(sequentialSizes(1));
 console.log(sequentialSizes(4));
 console.log(sequentialSizes(7));
 console.log(sequentialSizes(3));
+
+
+//TODO: You will write a card counting function. It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.

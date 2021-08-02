46b1331c5e7bb6c64311bfd38dfc3fc5b68233f5
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-08-01 23:25:41 -0500

    js basic practice

--- js/jsPractice.js
+++ js/jsPractice.js
@@ -77,3 +77,42 @@ function testElse(val) {
 
 console.log(testElse(4));
 console.log(testElse(8));
+
+//TODO: In the game of golf, each hole has a par, meaning, the average number of strokes a golfer is expected to make in order to sink the ball in the hole to complete the play. Depending on how far above or below par your strokes are, there is a different nickname.
+//
+// Your function will be passed par and strokes arguments. Return the correct string according to this table which lists the strokes in order of priority; top (highest) to bottom (lowest):
+//
+// Strokes	Return
+// 1	"Hole-in-one!"
+// <= par - 2	"Eagle"
+// par - 1	"Birdie"
+// par	"Par"
+// par + 1	"Bogey"
+// par + 2	"Double Bogey"
+// >= par + 3	"Go Home!"
+
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+    if(strokes === 1){
+        return names[0]
+    } else if(strokes <= par - 2){
+        return names[1]
+    } else if(strokes === par - 1){
+        return names[2]
+    } else if(strokes === par){
+        return names[3]
+    } else if(strokes === par + 1){
+        return names[4]
+    } else if(strokes === par + 2){
+        return names[5]
+    } else{
+        return names[6]
+    }
+
+    return "Change Me";
+}
+
+console.log(golfScore(5, 4));
+console.log(golfScore(1));
+console.log(golfScore(2, 4));
+console.log(golfScore(10, 5));

d59f432ae6a777a7c3177f6de31b17fb6506dd1a
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-08-01 23:49:45 -0500

    range switch case

--- js/jsPractice.js
+++ js/jsPractice.js
@@ -175,3 +175,34 @@ console.log(switchOfStuff(1));
 console.log(switchOfStuff("a"));
 console.log(switchOfStuff("b"));
 console.log(switchOfStuff("c"));
+
+//TODO: Write a switch statement to set answer for the following ranges:
+// 1-3 - Low
+// 4-6 - Mid
+// 7-9 - High
+
+function sequentialSizes(val) {
+    var answer = "";
+    switch(val){
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+    }
+    return answer;
+}
+
+console.log(sequentialSizes(1));
+console.log(sequentialSizes(4));
+console.log(sequentialSizes(7));
+console.log(sequentialSizes(3));

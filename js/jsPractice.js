7f744f79274181fa52079a0226fed402bb165c3a
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-08-02 22:50:59 -0500

    Counting cards exercise complete

--- js/jsPractice.js
+++ js/jsPractice.js
@@ -209,3 +209,35 @@ console.log(sequentialSizes(3));
 
 
 //TODO: You will write a card counting function. It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.
+
+var count = 0;
+
+function cc(card) {
+    switch(card){
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count ++;
+            break;
+        case 10:
+        case 'J':
+        case 'Q':
+        case 'K':
+        case 'A':
+            count --;
+            break;
+    }
+    if(count <= 0){
+        return count + " Hold";
+    } else {
+        return count + " Bet";
+    }
+}
+
+console.log(cc(2));
+console.log(cc(3));
+console.log(cc(7));
+console.log(cc('K'));
+console.log(cc('A'));

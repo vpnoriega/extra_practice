78eec3ff1fd2a12659952fd536c8d8eb159c5b39
Vanessa Noriega <vanessa.p.noriega@gmail.com>
2021-08-04 12:40:00 -0500

    Edited notes and added an array version

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,33 +1,5 @@
 // Print integers 1 to N, but print “Fizz” if an integer is divisible by 3, “Buzz” if an integer is divisible by 5, and “FizzBuzz” if an integer is divisible by both 3 and 5.
 
-for(let n=1; n<50; n++){
-    if(n%15 === 0){
-        console.log(n + "Fizzbuzz")
-    } else if(n%5 === 0){
-        console.log(n +"buzz")
-    } else if(n%3 === 0){
-        console.log(n +"fizz")
-    } else{
-        console.log(n)
-    }
-}
-
-for(let n=1; n<50; n++){
-    switch(true){
-        case n%5 === 0 && n%3 ===0:
-            console.log(n +"fizzbuzz");
-            break;
-        case n%5 === 0:
-            console.log(n +"buzz");
-            break;
-        case n%3 === 0:
-            console.log(n +"fizz");
-            break;
-        default:
-            console.log(n)
-    }
-}
-
 /** if (condition){
     // executed only if "condition" is true
 } else if (other condition){
@@ -36,9 +8,72 @@ for(let n=1; n<50; n++){
     // executed only if both "condition" and "other condition" were false
 }
  */
+function fizzBuzz() {
+    let result = "";
+    for (let n = 1; n <= 50; n++) {
+        if (n % 15 === 0) {
+            console.log("Fizzbuzz")
+        } else if (n % 5 === 0) {
+            console.log("buzz")
+        } else if (n % 3 === 0) {
+            console.log("fizz")
+        } else {
+            console.log(n)
+        }
+    }
+    return result;
+}
+
+console.log(fizzBuzz());
+
+
+/** As an ARRAY [] **/
+
+function fizzBuzzArr() {
+    var array = [];
+    for (var i = 1; i <= 100; i++) {
+        if (i % 15 === 0) {
+            array.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            array.push("Fizz");
+        } else if (i % 5 === 0) {
+            array.push("Buzz");
+        } else {
+            array.push(i);
+        }
+    }
+    return array;
+}
+
+console.log(fizzBuzzArr());
+
+
+/** Switch Statement**/
+
+function fizzBuzzSwitch() {
+    let resultSwitch = "";
+    for (let n = 1; n <= 50; n++) {
+        switch (true) {
+            case n % 5 === 0 && n % 3 === 0:
+                console.log("FizzBuzz");
+                break;
+            case n % 5 === 0:
+                console.log("Buzz");
+                break;
+            case n % 3 === 0:
+                console.log("Fizz");
+                break;
+            default:
+                console.log(n)
+        }
+    }
+    return resultSwitch;
+}
+
+console.log(fizzBuzzSwitch());
 
 /**
-switch (true){ //Switch statement if the condition is True (similar to if statement)
+ switch (true){ //Switch statement if the condition is True (similar to if statement)
     case x: //(else if)
         code block;
         break;
@@ -52,3 +87,4 @@ switch (true){ //Switch statement if the condition is True (similar to if statem
         code block;
 }
  */
+
